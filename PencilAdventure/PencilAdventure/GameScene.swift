961e88ff2d96c4a00c02ddcb7b0c2fffd46b59cd
Jocelyn Harrington <pangzee@gmail.com>
2014-07-29 00:19:58 -0700

    Create initial game scene. Add Start game button to view controller.

---
+++ PencilAdventure/PencilAdventure/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  PencilAdventure
+//
+//  Created by Jocelyn Harrington on 7/29/14.
+//  Copyright (c) 2014 backstopmedia. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene : SKScene {
+
+}
---
+++ PencilAdventure/PencilAdventure/GameViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GameViewController.swift
+//  PencilAdventure
+//
+//  Created by Jocelyn Harrington on 7/28/14.
+//  Copyright (c) 2014 backstopmedia. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    var scene: GameScene!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func startGame(sender : AnyObject) {
+        //load game scene assets
+    }
+}
