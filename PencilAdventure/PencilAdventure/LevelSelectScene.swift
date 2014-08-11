687dd8a6f44e0e34aba4b8e95335d1d7a48fc3b3
Clay McIlrath <clay.mcilrath@gmail.com>
2014-08-11 13:05:47 -0400

    nicer level selection icons

--- PencilAdventure/PencilAdventure/LevelSelectScene.swift
+++ PencilAdventure/PencilAdventure/LevelSelectScene.swift
@@ -13,62 +13,73 @@ class LevelSelectScene : SKScene {
 	private var progressLoader: ProgressLoaderNode!
 	private let MaxLevels = 8
 	
-    override func didMoveToView(view: SKView) {
-      scene.backgroundColor = UIColor.whiteColor()
-      addLevelSelectNode()
-    }
+  override func didMoveToView(view: SKView) {
+    scene.backgroundColor = UIColor.whiteColor()
+    addLevelSelectNode()
+  }
     
-    internal func addLevelSelectNode() {
-        SoundManager.toggleBackgroundMusic()
-		var atlas = SKTextureAtlas(named: "Sprites")
-		var blueTile = atlas.textureNamed("bluetile")
-
+  internal func addLevelSelectNode() {
+    addBackground()
+    SoundManager.toggleBackgroundMusic()
 		
-      var tileWidth = blueTile.size().width
-      var tileHeight = blueTile.size().height
-      var gap = tileWidth * 2
-		
-		var selectorWidth = tileWidth * CGFloat(MaxLevels) + gap * CGFloat(MaxLevels - 1)
-        var x = (view.frame.width - selectorWidth) / 2.0
-        var y = view.frame.height / 2
-        for i in 1...MaxLevels {
-            let level = SKSpriteNode(texture: blueTile)
-            level.name = "\(i)"
-			level.position =  CGPoint(x: x, y: y)
-			level.xScale = getSceneScaleX()
-			level.yScale = getSceneScaleY()
-            self.addChild(level)
-			
-			x += tileWidth + gap
-        }
+    let atlas = SKTextureAtlas(named: "Levels")
+		let levelTile = atlas.textureNamed("L1-enabled")
+    let tileWidth = levelTile.size().width
+    let tileHeight = levelTile.size().height
+    let gap = tileWidth / 5
+    var x = (view.frame.width - CGFloat(420)) / 2.0
+    var y = view.frame.height / 2
+    
+    for i in 1...MaxLevels {
+      var suffix = "disabled"
+      
+      if i == 1 || i == 2 {
+        suffix = "enabled"
+      }
+      
+      let level = SKSpriteNode(texture: atlas.textureNamed("L\(i)-\(suffix)"))
+      level.name = "\(i)"
+      level.position =  CGPoint(x: x, y: y)
+      level.xScale = getSceneScaleX()
+      level.yScale = getSceneScaleY()
+      
+      addChild(level)
+      
+      x += tileWidth + gap
+    }
 		
 		// TODO: SKLabelNode isn't available on iOS 7 - if we plan to support that, we might consider an alternative
 		let levelLabel = SKLabelNode(text: "Please choose a level")
 		levelLabel.fontColor = SKColor.darkGrayColor()
-        levelLabel.fontName = "Noteworthy"
-		levelLabel.position = CGPoint(x: view.frame.width / 2, y: y + tileHeight * 3)
+    levelLabel.fontName = "Noteworthy"
+		levelLabel.position = CGPoint(x: view.frame.width / 2, y: y + tileHeight)
 		levelLabel.xScale = getSceneScaleX()
 		levelLabel.yScale = getSceneScaleY()
-		self.addChild(levelLabel)
+    addChild(levelLabel)
         
-        if let highestScores = ScoreManager.getAllHighScores() {
-            let highScoreLabel = SKLabelNode(text: "High Score\n\(highestScores)")
-
-            highScoreLabel.fontColor = SKColor.darkGrayColor()
-            highScoreLabel.fontName = "Noteworthy"
-			highScoreLabel.position = CGPoint(x: view.frame.width / 2, y: y - highScoreLabel.frame.height)
-            highScoreLabel.xScale = getSceneScaleX()
-            highScoreLabel.yScale = getSceneScaleY()
-            addChild(highScoreLabel)
-        }
+    if let highestScores = ScoreManager.getAllHighScores() {
+      let highScoreLabel = SKLabelNode(text: "High Score\n\(highestScores)")
+      highScoreLabel.fontColor = SKColor.darkGrayColor()
+      highScoreLabel.fontName = "Noteworthy"
+      highScoreLabel.fontSize = 14
+			highScoreLabel.position = CGPoint(x: view.frame.width / 2, y: y - tileHeight - highScoreLabel.frame.height)
+      highScoreLabel.xScale = getSceneScaleX()
+      highScoreLabel.yScale = getSceneScaleY()
+      addChild(highScoreLabel)
     }
-  
+  }
   
   internal func addProgressLoaderNode () {
-	progressLoader = ProgressLoaderNode(scene: self)
+    progressLoader = ProgressLoaderNode(scene: self)
     progressLoader.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 2 + 100)
     progressLoader.setProgress(0)
     addChild(progressLoader)
+  }
+  
+  private func addBackground() {
+    let backgroundTexture = SKSpriteNode(color: UIColor(red: 154/255, green: 208/255, blue: 217/255, alpha: 1.0), size: frame.size)
+    backgroundTexture.position = CGPointMake(frame.width / 2, frame.height / 2)
+    addChild(backgroundTexture)
   }
 	
   func loadLevel(level: String) {
@@ -77,8 +88,8 @@ class LevelSelectScene : SKScene {
     
     SoundManager.restartBackgroundMusic()
 	
-	// Add our progress to the scene
-	addProgressLoaderNode()
+    // Add our progress to the scene
+    addProgressLoaderNode()
 	
     // Unarchive scene.
     work.append {
@@ -89,10 +100,12 @@ class LevelSelectScene : SKScene {
     // not block the main thread which renders the scene
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
       var done = 0
+    
       for job in work {
         done++
         self.progressLoader.setProgress(CGFloat(done) / CGFloat(work.count))
         job()
+      
         if done == work.count {
           // Notify the main that that we're ready!
           dispatch_async(dispatch_get_main_queue()) {
@@ -101,28 +114,31 @@ class LevelSelectScene : SKScene {
         }
       }
     }
-
   }
 
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-      for touch: AnyObject in touches {
-        let location = touch.locationInNode(self)
-        let node = self.nodeAtPoint(location)
-        var loaded = false
-        if let buttonName = node.name {
-			//TODO: add more levels
-			if buttonName == "1" {
-				loadLevel("1")
-				loaded = true
-			}
-			if buttonName == "2" {
-				loadLevel("2")
-				loaded = true
-			}
+  override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+    for touch: AnyObject in touches {
+      let location = touch.locationInNode(self)
+      let node = self.nodeAtPoint(location)
+      var loaded = false
+      
+      if let buttonName = node.name {
+        // TODO: add more levels
+        if buttonName == "1" {
+          loadLevel("1")
+          loaded = true
         }
-        if !loaded {
-          loadLevel("GameScene")
+
+        if buttonName == "2" {
+          loadLevel("2")
+          loaded = true
         }
       }
+      
+      if !loaded {
+        loadLevel("GameScene")
+      }
     }
+  }
+  
 }
