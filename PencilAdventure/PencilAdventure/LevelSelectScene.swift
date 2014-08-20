76381a2e0ba73887effbaddc2162947d677e92e4
Christoffer Hallas <christoffer.hallas@gmail.com>
2014-08-20 15:59:40 -0400

    added game over scene

--- PencilAdventure/PencilAdventure/LevelSelectScene.swift
+++ PencilAdventure/PencilAdventure/LevelSelectScene.swift
@@ -14,6 +14,7 @@ class LevelSelectScene : SKScene {
     private let MaxLevels = 4
     
     override func didMoveToView(view: SKView) {
+      super.didMoveToView(view)
         scene.backgroundColor = UIColor.whiteColor()
         addLevelSelectNode()
     }
