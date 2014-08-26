58cdd438f5bef58c1f54e38dd720626655eb79c7
Ankur Patel <ankur.patel@ymail.com>
2014-08-25 21:47:13 -0400

    Updating start screen

--- PencilAdventure/PencilAdventure/GameViewController.swift
+++ PencilAdventure/PencilAdventure/GameViewController.swift
@@ -9,32 +9,40 @@
 import SpriteKit
 
 class GameViewController: UIViewController {
+    
+    @IBOutlet var startGameButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Setup our SpriteKit view.
+        let view = self.view as SKView
+        //		view.showsFPS = true
+        //		view.showsNodeCount = true
+        view.ignoresSiblingOrder = true
 
-  @IBOutlet var startGameButton: UIButton!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    // Setup our SpriteKit view.
-		let view = self.view as SKView
-//		view.showsFPS = true
-//		view.showsNodeCount = true
-		view.ignoresSiblingOrder = true
-
-    // Start the background music.
-    SoundManager.playBackgroundMusic()
-  }
-	
-  @IBAction func startGame (sender: AnyObject) {
-    let view = self.view as SKView
-
-    // Create and present our level scene.
-    let levelScene = LevelSelectScene(size: CGSize(width: view.frame.width, height: view.frame.height))
-    view.presentScene(levelScene)
-		
-    // We started the game, so hide the start game button.
-    startGameButton.hidden = true
-  }
-
+        
+        let startScene = SKScene()
+        startScene.backgroundColor = GAME_BLUE_COLOR
+        view.presentScene(startScene)
+        
+        // Start the background music.
+        SoundManager.playBackgroundMusic()
+    }
+    
+    @IBAction func startGame (sender: AnyObject) {
+        let view = self.view as SKView
+        
+        // Create and present our level scene.
+        let levelScene = LevelSelectScene(size: CGSize(width: view.frame.width, height: view.frame.height))
+        view.presentScene(levelScene)
+        
+        for subview in view.subviews {
+            if let sView = subview as? UIView {
+                sView.hidden = true
+            }
+        }
+    }
+    
 }
 
