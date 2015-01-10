060ebe8b936883b730b7734a6b0674ce6bee3a4b
Ankur Patel <ankur.patel@ymail.com>
2015-01-09 23:37:05 -0500

    Adding Workspace to encapsulate project and playgrounds

--- Playgrounds/PlatformGame.playground/section-2.swift
+++ Playgrounds/PlatformGame.playground/section-2.swift
@@ -1,3 +1,6 @@
+// Images do not load in Playground in Xcode 6.1
+// Please wait for the next version or try this playground in an older 6.0.1 version of Xcode
+
 // PlatformGame Playground
 import UIKit
 import SpriteKit
